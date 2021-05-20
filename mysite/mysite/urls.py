a3fec7d4499119969618927aa30f7a2c615f2d9d
Mairon <mariusz.rogawski@gmail.com>
2021-05-20 10:12:32 +0200

    Front-End is done for this moment

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('torg.urls')),
+    path('', include('torg.urls')),
 ]
 
 if settings.DEBUG:
